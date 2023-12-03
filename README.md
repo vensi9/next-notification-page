20426750e9bc76a5aea5db780e0ffad0da391efd
vensi <tejanivensi@gmail.com>
2023-12-03 22:50:51 +0530

    Extended the notification page with Tailwind CSS

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Notifications Page Solution
+
+
+## Table of Contents
+
+- [Notifications Page Solution](#notifications-page-solution)
+  - [Table of Contents](#table-of-contents)
+  - [Getting Started](#getting-started)
+  - [Overview](#overview)
+    - [Solution Screenshot](#solution-screenshot)
+    - [Links](#links)
+  - [Development Process](#development-process)
+    - [Built with](#built-with)
+  - [Author](#author)
 
 ## Getting Started
 
@@ -6,29 +19,27 @@ First, run the development server:
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Overview
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+### Solution Screenshot
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+![Solution Screenshot](./design/result.png)
 
-## Learn More
+### Links
 
-To learn more about Next.js, take a look at the following resources:
+- Solution URL: [GitHub](https://github.com/vensi9/next-notification-page)
+- Live Site URL: [Live Site](https://notification-page-next-tailwind.netlify.app/)
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## Development Process
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+### Built with
 
-## Deploy on Vercel
+- Next.js
+- Tailwind CSS
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+## Author
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- Frontend Mentor - [Vensi Tejani](https://www.frontendmentor.io/profile/vensi9)
+- Twitter - [@9_Vensi](https://twitter.com/9_Vensi)
--- app/page.js
+++ app/page.js
@@ -1,29 +1,28 @@
-
 export default function Home() {
   return (
     <>
-      <nav className="flex items-center justify-between px-10 py-5 font-plus-jakarta-sans font-500 text-16px">
+      <nav className="flex items-center justify-between px-5 py-5 sm:px-10 font-plus-jakarta-sans font-500 text-16px">
         <div className="flex items-center ">
-          <span className="text-2xl  sm:font-[.625rem] text-very-dark-blue font-semibold mr-2">Notifications</span>
+          <span className="sm:text-2xl text-[1.2rem]  sm:font-[.625rem] text-very-dark-blue font-semibold mr-2">Notifications</span>
           <span className=" font-800  rounded-[4px] bg-Blue text-White text-xs w-6 h-5 flex items-center justify-center">
             3
           </span>
         </div>
-        <span className="cursor-pointer">Mark all as read</span>
+        <span className="cursor-pointer text-[0.9rem] sm:text-[1rem] text-dark-grayish-blue hover:text-[#0a317b]">Mark all as read</span>
       </nav>
 
 
-      <div className=" mx-10 grid gap-3">
-        <div className="flex py-2 px-5 bg-very-light-grayish-blue shadow-md rounded-lg items-center">
+      <div className=" mx-5 sm:mx-10 grid gap-3 font-plus-jakarta-sans ">
+        <div className="flex py-2 px-5 bg-very-light-grayish-blue  rounded-lg items-center">
           <div className="w-14 h-14 flex-shrink-0 rounded-full overflow-hidden">
             <img src="./assets/images/avatar-mark-webber.webp" alt="Profile Image" className="w-full h-full object-cover" />
           </div>
           <div className="m-4 flex-grow ">
             <div>
-              <span className="font-800 cursor-pointer">Mark Webber </span>
-              <span className="dark-grayish-blue">reacted to your recent post </span>
-              <span className="font-semibold inline-flex items-center gap-1 cursor-pointer">My first tournament today!
-                <span className="w-1.5 h-1.5 bg-Red rounded-full"></span>
+              <span className="hover:text-[#0a317b] font-800  text-[0.95rem] sm:text-[1rem] text-very-dark-blue cursor-pointer ">Mark Webber </span>
+              <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">reacted to your recent post </span>
+              <span className="hover:text-[#0a317b] font-800 text-dark-grayish-blue text-[0.95rem] sm:text-[1rem] inline-flex items-center gap-1 cursor-pointer">My first tournament today!
+                <span className="sm:[w-1.5 h-1.5] w-2 h-2 bg-Red rounded-full"></span>
               </span>
             </div>
 
@@ -31,35 +30,35 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex px-5 py-2 bg-[#cee5ed36] shadow-md rounded-lg items-center">
+        <div className="flex px-5 py-2 bg-[#cee5ed36]  rounded-lg items-center">
           <div className="w-14 h-14 flex-shrink-0 rounded-full overflow-hidden">
             <img src="./assets/images/avatar-angela-gray.webp" alt="" className="w-full h-full object-cover" />
           </div>
           <div className="m-4 flex-grow">
             <div>
-              <span className="font-semibold cursor-pointer">Angela Gray </span>
-              <span className="text-gray-600 text-sm inline-flex items-center gap-1">commented on your post
-                <span className="w-1.5 h-1.5  bg-Red rounded-full "></span>
+              <span className="hover:text-[#0a317b] font-800 text-[0.95rem] sm:text-[1rem] text-very-dark-blue cursor-pointer">Angela Gray </span>
+              <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem] inline-flex items-center gap-1">commented on your post
+                <span className="sm:[w-1.5 h-1.5] w-2 h-2 bg-Red rounded-full "></span>
               </span>
             </div>
-            <div className="text-gray-400 text-xs">1m ago</div>
+            <div className="text-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">1m ago</div>
 
           </div>
         </div>
 
-        <div className="flex px-5 py-2 bg-[#cee5ed36] shadow-md rounded-lg items-center">
+        <div className="flex px-5 py-2 bg-[#cee5ed36]  rounded-lg items-center">
           <div className="w-14 h-14 flex-shrink-0 rounded-full overflow-hidden">
             <img src="./assets/images/avatar-jacob-thompson.webp" alt="" className="w-full h-full object-cover" />
           </div>
           <div className="m-4 flex-grow">
             <div>
-              <span className="font-semibold cursor-pointer">Jacob Thompson</span>
-              <span className="text-gray-600 text-sm">has joined your group</span>
-              <span className="font-semibold inline-flex items-center gap-1 cursor-pointer">Chess Club
-                <span className="w-1.5 h-1.5  bg-Red rounded-full"></span>
+              <span className="hover:text-[#0a317b] font-800 text-very-dark-blue cursor-pointer">Jacob Thompson</span>
+              <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]"> has joined your group </span>
+              <span className="hover:text-[#0a317b] font-800 text-dark-grayish-blue text-[0.95rem] sm:text-[1rem] inline-flex items-center gap-1 cursor-pointer"> Chess Club
+                <span className="sm:[w-1.5 h-1.5] w-2 h-2 bg-Red rounded-full"></span>
               </span>
             </div>
-            <div className="text-gray-400 text-xs">1 day ago</div>
+            <div className="text-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">1 day ago</div>
           </div>
         </div>
 
@@ -70,14 +69,14 @@ export default function Home() {
             </div>
             <div className="m-4 flex-grow">
               <div>
-                <span className="font-semibold cursor-pointer">Rizky Hasanuddin</span>
-                <span className="text-gray-600 text-sm">sent you a private message</span>
+                <span className="hover:text-[#0a317b] font-800 text-very-dark-blue cursor-pointer">Rizky Hasanuddin</span>
+                <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]"> sent you a private message</span>
               </div>
-              <div className="text-gray-400 text-xs">5 days ago</div>
+              <div className="text-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">5 days ago</div>
             </div>
           </div>
-          <div className="cursor-pointer border flex ml-[4.50rem] px-5 py-3 bg-white border-gray-300 rounded-lg items-center">
-            <p className="text-gray-400 text-sm">Hello, thanks for setting up the Chess Club. I've been a member for a few weeks
+          <div className="cursor-pointer border flex ml-[4.50rem] px-5 py-3 bg-white hover:bg-light-grayish-blue-1 border-light-grayish-blue-2 rounded-lg items-center">
+            <p className="font-500 text-[0.95rem] sm:text-[1rem] text-dark-grayish-blue">Hello, thanks for setting up the Chess Club. I've been a member for a few weeks
               now and
               I'm already having lots of fun and improving my game.</p>
           </div>
@@ -92,10 +91,13 @@ export default function Home() {
           </div>
           <div className="m-4 flex-grow">
             <div>
-              <span className="font-semibold cursor-pointer">Kimberly Smith</span>
-              <span className="text-gray-600 text-sm">commented on your picture</span>
+              <span className="hover:text-[#0a317b] font-800 text-very-dark-blue cursor-pointer">Kimberly Smith</span>
+              <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]"> commented on your picture</span>
             </div>
-            <div className="text-gray-400 text-xs">1 week ago</div>
+            <div className="text-grayish-blue font-500 text-[0.95rem] sm:text-[1rem] ">1 week ago</div>
+          </div>
+          <div className="w-14 h-14 flex-shrink-0 overflow-hidden order-last md:order-last">
+            <img src="./assets/images/image-chess.webp" alt="" className="w-full h-full object-cover cursor-pointer" />
           </div>
         </div>
 
@@ -105,11 +107,11 @@ export default function Home() {
           </div>
           <div className="m-4 flex-grow">
             <div>
-              <span className="font-semibold cursor-pointer">Nathan Peterson</span>
-              <span className="text-gray-600 text-sm"> reacted to your recent post</span>
-              <span className="font-semibold cursor-pointer">5 end-game strategies to increase your win rate</span>
+              <span className="hover:text-[#0a317b] font-800 text-very-dark-blue cursor-pointer">Nathan Peterson</span>
+              <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]"> reacted to your recent post</span>
+              <span className="hover:text-[#0a317b] font-800 text-dark-grayish-blue text-[0.95rem] sm:text-[1rem] cursor-pointer"> 5 end-game strategies to increase your win rate</span>
             </div>
-            <div className="text-gray-400 text-xs">2 weeks ago</div>
+            <div className="text-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">2 weeks ago</div>
           </div>
         </div>
 
@@ -119,18 +121,20 @@ export default function Home() {
           </div>
           <div className="m-4 flex-grow">
             <div>
-              <span className="font-semibold cursor-pointer">Anna Kim</span>
-              <span className="text-gray-600 text-sm">left the group</span>
-              <span className="font-semibold cursor-pointer">Chess Club</span>
+              <span className="hover:text-[#0a317b] font-800 text-very-dark-blue cursor-pointer">Anna Kim </span>
+              <span className="text-dark-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">left the group </span>
+              <span className="hover:text-[#0a317b] font-800 text-dark-grayish-blue text-[0.95rem] sm:text-[1rem] cursor-pointer">Chess Club</span>
             </div>
-            <div className="text-gray-400 text-xs">2 weeks ago</div>
+            <div className="text-grayish-blue font-500 text-[0.95rem] sm:text-[1rem]">2 weeks ago</div>
           </div>
         </div>
-
       </div>
-      <div className="attribution mt-5">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Vensi Tejani</a>.
+
+      <div className="flex justify-center align-items-center my-10 text-[0.9rem]">
+        Challenge by
+        <a className="text-[#3e52a3] hover:text-[#0a317b] font-800 ml-1" href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>. 
+        Coded by 
+        <a href="https://github.com/vensi9/next-notification-page"  className="ml-1 text-[#3e52a3] hover:text-[#0a317b] font-800"> Vensi Tejani</a>.
       </div>
     </>
   )
